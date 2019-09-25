5a64ece46e4f31263ee8532985d79c74606bb76f
Yuna Suzuki <yuna.com.jp@outlook.jp>
2019-09-25 21:39:44 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 鈴木裕奈 on 2019/09/25.
+//  Copyright © 2019 Yuna Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
